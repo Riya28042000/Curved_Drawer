128472a1c03f81162e7d2968c409348ae9f5fe09
Riya28042000 <gargriya28042000@gmail.com>
2020-05-29 02:22:28 +0530

    curved_drawer

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:drawer/home/curved.dart';
+
+
+void main() {
+//   List<DrawerItem>items=[DrawerItem(icon: Icon(Icons.home),label: "Riya Garg"),
+//  DrawerItem(icon: Icon(Icons.hot_tub),label: "Shashank"),
+//  DrawerItem(icon: Icon(Icons.hotel),label: "Anshul Sahai"),
+//  DrawerItem(icon: Icon(Icons.http),label: "Appu Garg"),
+//  DrawerItem(icon: Icon(Icons.image),label: "Nautanki sala")];
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:DrawerExample()));
+}
+
+// List<DrawerItem> listpages = [
+        
+//         DrawerItem(
+//           icon: Icon(Icons.library_books),
+//           label:"News"
+//         ),
+//         DrawerItem(
+//           icon: Icon(Icons.calendar_today),
+//           label: "Schedule"
+//         ),
+//          DrawerItem(
+//           icon: Icon(Icons.home),
+//           label:"Home"
+//         ),
+//          DrawerItem(
+//           icon: Icon(Icons.person_add),
+//           label:"Register"
+//         ),
+//         DrawerItem(
+//           icon: Icon(Icons.people),
+//           label:"About Us"
+//         ),
+
+    
+//   ];
