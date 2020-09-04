4ec768e76f03b8c2bb7a3e92ff15b5455cdace82
Riya Garg <65823846+Riya28042000@users.noreply.github.com>
2020-09-05 01:12:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,3 @@ void main() {
     
 }
 
-// List<DrawerItem> listpages = [
-        
-//         DrawerItem(
-//           icon: Icon(Icons.library_books),
-//           label:"News"
-//         ),
-//         DrawerItem(
-//           icon: Icon(Icons.calendar_today),
-//           label: "Schedule"
-//         ),
-//          DrawerItem(
-//           icon: Icon(Icons.home),
-//           label:"Home"
-//         ),
-//          DrawerItem(
-//           icon: Icon(Icons.person_add),
-//           label:"Register"
-//         ),
-//         DrawerItem(
-//           icon: Icon(Icons.people),
-//           label:"About Us"
-//         ),
-
-    
-//   ];
